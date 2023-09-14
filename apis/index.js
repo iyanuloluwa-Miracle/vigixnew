2e2259b942c05254e0a64e9e1c23397ce5a89e05
haywhyvilla <olatunjiayobami575@gmail.com>
2023-09-14 13:35:47 +0100

    new request

--- apis/index.js
+++ apis/index.js
@@ -31,13 +31,13 @@ const api = {
 
   fetchIncidents: (token, query) => {
     const headers = createHTTPHeader(token);
-    const url = `${BASE_URL}/incident//incident-statuses/16`;
+    const url = `${BASE_URL}/incident/incident-statuses/16`;
     return api.get(url, headers);
   },
 
   fetchNPFIncidents: (token, query) => {
     const headers = createHTTPHeader(token);
-    const url = `${BASE_URL}/incident/incident-statuses/18`;
+    const url = `${BASE_URL}/incident/police-incidents/10,18`;
     return api.get(url, headers);
   },
 
